443fca745ba7ec1564a90a820e7f82256b0ddc24
NOSU-WEB <79894331+nosu-web@users.noreply.github.com>
2021-11-25 00:52:53 +0300

    Create main.css

---
+++ css/main.css
@@ -0,0 +1,19 @@
+.minesweeper {
+    margin: auto;
+    border-spacing: 0;
+    border-collapse: collapse;
+}
+.minesweeper-cell {
+    padding: 0;
+    width: 60px;
+    height: 60px;
+}
+.mined {
+    background-image: url('../icons/mine.png');
+    background-size: contain;
+}
+.minesweeper-button {
+    border-radius: 0;
+    width: 60px;
+    height: 60px;
+}
